17ab5eaeb3ab294e5d64c83739765c7b367ca5b2
oyevamos <deadhead2520@gmail.com>
2024-04-26 12:37:23 +0300

    task3 is done

---
+++ task3/README.md
@@ -0,0 +1,4 @@
+https://acmp.ru/index.asp?main=task&id_task=43
+
+сложность - O(n)
+память - O(n)
--- task3/main.go
+++ task3/main.go
@@ -3,41 +3,43 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 )
 
 func main() {
 	file, err := os.Open("INPUT.TXT")
 	if err != nil {
-		fmt.Println("Ошибка при чтении файла")
+		fmt.Println("Ошибка при чтении файла:", err)
 		return
 	}
 	defer file.Close()
 
 	cnt := 0
-	max := -11111
+	max := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		for i := 0; i < len(line); i++ {
-			// fmt.Println(string(line[i]))
-			if string(line[i]) == "0" {
+		for _, char := range line {
+			if char == '0' {
 				cnt++
-				max = int(math.Max(float64(max), float64(cnt)))
+				if cnt > max {
+					max = cnt
+				}
 			} else {
 				cnt = 0
 			}
 		}
 	}
-	// fmt.Println(max)
+
 	outputFile, err := os.Create("OUTPUT.TXT")
 	if err != nil {
-		fmt.Println("Ошибка при создании файла для записи ", err)
+		fmt.Println("Ошибка при создании файла для записи:", err)
 		return
 	}
 	defer outputFile.Close()
-	// buba := strconv.Itoa(max)
-	_, err = fmt.Fprintln(outputFile, max)
+
+	if _, err = fmt.Fprintln(outputFile, max); err != nil {
+		fmt.Println("Ошибка при записи в файл:", err)
+	}
 }
